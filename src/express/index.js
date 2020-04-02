7728640a8102a4e11afcb4283de3e1f3237d19bb
Eugeno <eugeneath@yandex.ru>
2020-04-02 20:11:36 +0700

    module3-task2

--- src/express/index.js
+++ src/express/index.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
+
+const PUBLIC_DIR = `public`;
 
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
@@ -9,6 +12,10 @@ const articlesRoutes = require(`./routes/articles-routes`);
 const DEFAULT_PORT = 8080;
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, PUBLIC_DIR, `templates`));
+app.set(`view engine`, `pug`);
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
